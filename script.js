7b1c4d45258f2bc26d05653c5699ec095f05d1fe
Hemstreet <jonhemstreet@gmail.com>
2015-05-26 23:40:19 -0600

    gitignore, changed pin read

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var gpio = require("pi-gpio"),
     delay = 600000,
     toggleLights = null;
 
-gpio.read(0, function(err, value) {
+gpio.read(11, function(err, value) {
     if(err) throw err;
     console.log(value); // The current state of the pin
 });
