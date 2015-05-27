4b23f3f94aac8c43de12699e0e3fdbc669bdc799
Hemstreet <jonhemstreet@gmail.com>
2015-05-26 23:31:03 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var gpio = require("pi-gpio"),
+    delay = 600000,
+    toggleLights = null;
+
+gpio.read(0, function(err, value) {
+    if(err) throw err;
+    console.log(value); // The current state of the pin
+});
+
+function myFunction() {
+    toggleLights = setTimeout(function(){ alert("Hello"); }, 3000);
+}
+
+function myStopFunction() {
+    clearTimeout(toggleLights);
+}
+
+//toggleLights = setTimeout("javascript function", delay);
